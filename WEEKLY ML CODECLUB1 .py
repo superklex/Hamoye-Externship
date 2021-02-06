6916e09035b1caec7c1fe527d43341b8a3d607a9
superklex <kosiuhuegbu@gmail.com>
2021-02-06 15:46:54 +0100

    2

--- WEEKLY ML CODECLUB1 .py
+++ WEEKLY ML CODECLUB1 .py
@@ -1,1 +1,9 @@
 import pandas as pd
+cell = pd.read_csv("cellphones.csv")
+population = pd.read_csv("population.csv")
+cell_pop = cell.merge(population, how ="inner", on =["Country", "year"])
+cell_pop.head()
+geo = pd.read_csv("geo_country.csv")
+geo.rename(columns={"country": "geo", "name": "Country"}, inplace = True)
+geo.columns
+
